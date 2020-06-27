b5ba0d7994bb1f693f14207b9965051d18549c8e
Chyna Davis <chyna.davis11@gmail.com>
2020-06-27 16:47:19 -0700

    add to user and post models

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 const bcrypt = require("bcryptjs");
 
 // Creating our User model
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   const User = sequelize.define(
     "User",
     {
@@ -12,42 +12,58 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false,
         unique: true,
         validate: {
-          isEmail: true
-        }
+          isEmail: true,
+        },
       },
+      // username (required for login/signup)
       username: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        len: [3, 25],
       },
       // The password cannot be null
       password: {
         type: DataTypes.STRING,
-        allowNull: false
-      }
+        allowNull: false,
+      },
+      // user's profile pic taken from a Cloudinary URL
+      profilePic: {
+        type: DataTypes.STRING,
+        isUrl: true,
+      },
+      // user bio; length limit of 200 characters
+      biography: {
+        type: DataTypes.TEXT,
+        len: [1, 200],
+      },
+      userLinks: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        isUrl: true,
+      },
     },
     {
       // This forces any default 'User' to exclude the password when we query them;
       // this way we don't expose even a hashed password
       defaultScope: {
-        attributes: { exclude: ["password"] }
+        attributes: { exclude: ["password"] },
       },
       // If you want to show the password, for whatever reason, we expose with:
       // db.User.scope('withPassword').findAll() etc
       scopes: {
         withPassword: {
-          attributes: {}
-        }
-      }
+          attributes: {},
+        },
+      },
     }
   );
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", function(user) {
+  User.addHook("beforeCreate", function (user) {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
@@ -55,11 +71,11 @@ module.exports = function(sequelize, DataTypes) {
     );
   });
 
-  User.associate = function(models) {
+  User.associate = function (models) {
     // Associating User with Posts
     // When an User is deleted, also delete any associated Posts
     User.hasMany(models.Post, {
-      onDelete: "cascade"
+      onDelete: "cascade",
     });
   };
 
