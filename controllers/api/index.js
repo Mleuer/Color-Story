360ddfc88c10682482255a9d3f3efd2c646db905
Elizabeth Munoz <munoz_714@live.com>
2020-06-27 13:43:10 -0700

    add routes to index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,14 @@ const router = require("express").Router();
 // Import our controllers
 const postRoutes = require("./postsController");
 const userRoutes = require("./usersController");
+const userRoutes = require("./messagesController");
+const userRoutes = require("./conversationController");
 
 // Hook up to the router
 router.use("/posts", postRoutes);
 router.use("/users", userRoutes);
+router.use("/messages", userRoutes);
+router.use("/conversation", userRoutes);
 
 // Export the router
 module.exports = router;
