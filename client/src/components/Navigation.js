54da8c6eecc776f432516f52bee36fb8997b452d
Matt Leuer <mattleuer@gmail.com>
2020-06-29 18:00:11 -0700

    Post data is stored in state

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Button, AppBar, Toolbar } from '@material-ui/core';
+import { Button, AppBar, Toolbar } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles ({
+const useStyles = makeStyles({
   bar: {
     backgroundColor: "pink",
     borderRadius: "24px",
   },
-})
-
-
+});
 
 function Navigation(props) {
   const { user, logoutUser } = props;
@@ -19,23 +17,34 @@ function Navigation(props) {
     <>
       <AppBar position="static" className={classes.bar}>
         <Toolbar>
-          <Button component={Link} to="/home" color="inherit">Home</Button>
-          <Button component={Link} to="/dashboard" color="inherit">Dashboard</Button>
+          <Button component={Link} to="/home" color="inherit">
+            Home
+          </Button>
 
-          {user.email ?
+          {user.email ? (
             <>
-              <Button component={Link} to="/home" onClick={logoutUser} color="inherit">Logout</Button>
+              <Button component={Link} to="/dashboard" color="inherit">
+                Dashboard
+              </Button>
+
+              <Button component={Link} to="/home" onClick={logoutUser} color="inherit">
+                Logout
+              </Button>
             </>
-            :
+          ) : (
             <>
-              <Button component={Link} to="/login" color="inherit">Login</Button>
-              <Button component={Link} to="/signup" color="inherit">Signup</Button>
+              <Button component={Link} to="/login" color="inherit">
+                Login
+              </Button>
+              <Button component={Link} to="/signup" color="inherit">
+                Signup
+              </Button>
             </>
-          }
+          )}
         </Toolbar>
       </AppBar>
     </>
-  )
+  );
 }
 
 export default Navigation;
