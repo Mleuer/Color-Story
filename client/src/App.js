c5cd957d3cc13a20cfb5a56fe850c3625ff9446a
Matt Leuer <mattleuer@gmail.com>
2020-07-08 22:57:03 -0700

    Liked posts display on the user page.

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
-import { Home, UserPost, Profile, ColorWall } from "./pages";
+import { Home, UserPost, Profile, ColorWall, Favorite } from "./pages";
 import Auth from "./pages/Auth"
 import { Navigation, Error } from "./components";
 import Container from '@material-ui/core/Container';
@@ -93,6 +93,9 @@ function App() {
                 </PrivateRoute>
                 <PrivateRoute exact user={user} path={["/profile"]}>
                   <Profile user={user} />
+                </PrivateRoute>
+                <PrivateRoute exact user={user} path={["/favorites"]}>
+                  <Favorite user={user} />
                 </PrivateRoute>
                 <Route exact path={["/login", "/signup"]}>
                   <Auth
