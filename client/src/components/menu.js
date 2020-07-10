fd89eaeab5f337e03692d8f399745697c587c19d
Elizabeth Munoz <munoz_714@live.com>
2020-07-09 17:10:09 -0700

    delete profile post/ create post refresh page add message

---
+++ client/src/components/menu.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import API from "../utils/API.js"
+
+const options = [
+    'Delete',
+
+];
+
+const ITEM_HEIGHT = 48;
+
+export default function LongMenu(props) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = (id) => {
+        console.log("close happening", id)
+        API.Post.delete(id).then(
+            function (data) {
+                console.log("data", data)
+                props.resetPost()
+            }
+        )
+        setAnchorEl(null);
+    };
+    var style = {
+        container: {
+            float: "right",
+            position: "relative",
+            bottom: "66px",
+            right: "5px",
+            zIndex: "100",
+            background: "white",
+            borderRadius: "61px",
+        }
+    }
+
+    return (
+        <div style={style.container} >
+            <IconButton
+                aria-label="more"
+                aria-controls="long-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                <MoreVertIcon />
+            </IconButton>
+            <Menu
+                id="long-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                    style: {
+                        maxHeight: ITEM_HEIGHT * 4.5,
+                        width: '20ch',
+                    },
+                }}
+            >
+                {options.map((option) => (
+                    <MenuItem key={option} selected={option === 'Pyxis'} onClick={() => { handleClose(props.id) }}>
+                        {option}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </div >
+    );
+}
