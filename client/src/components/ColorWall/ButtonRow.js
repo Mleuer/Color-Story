b372c72a1d9d127c5adea3219ac48c06ae2fdf7d
Linus41 <schief.craft@gmail.com>
2020-07-11 14:02:32 -0700

    changed styling buttonrow

--- client/src/components/ColorWall/ButtonRow.js
+++ client/src/components/ColorWall/ButtonRow.js
@@ -7,9 +7,12 @@ const useStyles = makeStyles({
   navRoot: {
     minWidth: 275,
     textAlign: "center",
-    backgroundColor: "lightgray",
+    backgroundColor: "gray",
     border: "2px solid black",
     paddingTop: "8px",
+    borderRadius: "24px",
+    marginTop: "15px",
+    marginBottom: "10px",
   },
   iconColor: {
     borderRadius: "20px"
@@ -19,14 +22,14 @@ const useStyles = makeStyles({
 function ButtonRow(props) {
   const classes = useStyles();
   const colors = [
-    {colorCat: "red", styleBack: "red", styleColor: "red"},
-    {colorCat: "orange", styleBack: "orange", styleColor: "orange"},
-    {colorCat: "yellow", styleBack: "yellow", styleColor: "yellow"},
-    {colorCat: "green", styleBack: "green", styleColor: "green"},
-    {colorCat: "blue", styleBack: "blue", styleColor: "blue"},
-    {colorCat: "purple", styleBack: "purple", styleColor: "purple"},
-    {colorCat: "black", styleBack: "black", styleColor: "black"},
-    {colorCat: "white", styleBack: "white", styleColor: "white"},
+    {colorCat: "#f22c4a", styleBack: "#f22c4a", styleColor: "#f22c4a"},
+    {colorCat: "#f97116", styleBack: "#f97116", styleColor: "#f97116"},
+    {colorCat: "#f9d916", styleBack: "#f9d916", styleColor: "#f9d916"},
+    {colorCat: "#0fd43a", styleBack: "#0fd43a", styleColor: "#0fd43a"},
+    {colorCat: "#37c6e5", styleBack: "#37c6e5", styleColor: "#37c6e5"},
+    {colorCat: "#4c24f9", styleBack: "#4c24f9", styleColor: "#4c24f9"},
+    {colorCat: "#b876f0", styleBack: "#b876f0", styleColor: "#b876f0"},
+    {colorCat: "#2d2a2f", styleBack: "#2d2a2f", styleColor: "#2d2a2f"},
     {colorCat: "multicolor", styleBack: "linear-gradient(45deg, red, green, blue)", styleColor: "gray"}
   ];
 
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -14,7 +14,6 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import API from "../../utils/API";
-import "./style.css";
 import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
