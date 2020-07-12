b7232fb35566eb63d5bc1129a5a2d911b35d2cf4
Chyna Davis <chyna.davis11@gmail.com>
2020-07-11 18:38:39 -0700

    fix ButtonRow filters; all ColorWalls are working again

--- client/src/components/ColorWall/ButtonRow.js
+++ client/src/components/ColorWall/ButtonRow.js
@@ -22,14 +22,14 @@ const useStyles = makeStyles({
 function ButtonRow(props) {
   const classes = useStyles();
   const colors = [
-    {colorCat: "#f22c4a", styleBack: "#f22c4a", styleColor: "#f22c4a"},
-    {colorCat: "#f97116", styleBack: "#f97116", styleColor: "#f97116"},
-    {colorCat: "#f9d916", styleBack: "#f9d916", styleColor: "#f9d916"},
-    {colorCat: "#0fd43a", styleBack: "#0fd43a", styleColor: "#0fd43a"},
-    {colorCat: "#37c6e5", styleBack: "#37c6e5", styleColor: "#37c6e5"},
-    {colorCat: "#4c24f9", styleBack: "#4c24f9", styleColor: "#4c24f9"},
-    {colorCat: "#b876f0", styleBack: "#b876f0", styleColor: "#b876f0"},
-    {colorCat: "#2d2a2f", styleBack: "#2d2a2f", styleColor: "#2d2a2f"},
+    {colorCat: "red", styleBack: "#f22c4a", styleColor: "#f22c4a"},
+    {colorCat: "orange", styleBack: "#f97116", styleColor: "#f97116"},
+    {colorCat: "yellow", styleBack: "#f9d916", styleColor: "#f9d916"},
+    {colorCat: "green", styleBack: "#0fd43a", styleColor: "#0fd43a"},
+    {colorCat: "blue", styleBack: "#197cbe", styleColor: "#197cbe"},
+    {colorCat: "purple", styleBack: "#b876f0", styleColor: "#b876f0"},
+    {colorCat: "black", styleBack: "#2d2a2f", styleColor: "#2d2a2f"},
+    {colorCat: "white", styleBack: "#f5fafd", styleColor: "#f5fafd"},
     {colorCat: "multicolor", styleBack: "linear-gradient(45deg, red, green, blue)", styleColor: "gray"}
   ];
 
