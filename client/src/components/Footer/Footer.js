2531d8e3cad660085339c198c97b1d9926c7242e
Chyna Davis <chyna.davis11@gmail.com>
2020-07-22 15:35:51 -0700

    fix footer links logout bug

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,22 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary">
       {"Copyright © "}
-      <Link
-        color="inherit"
+      <Typography
+        component={Link}
+        to={"https://github.com/Mleuer/Color-Story"}
         target="__blank"
-        href="https://github.com/Mleuer/Color-Story"
-        style={{ fontWeight: "bold" }}
+        style={{ fontWeight: "bold", color: "gray" }}
       >
         Color Story
-      </Link>{" "}
+      </Typography>{" "}
       2020
     </Typography>
   );
@@ -32,65 +32,62 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#FFCCCC",
   },
   memberText: {
-    fontWeight: "bold",
+    display: "flex",
+    justifyContent: "center",
     fontSize: "15px",
-    [theme.breakpoints.down("xs")]: {
-      fontSize: "10px",
-    },
   },
   memberLink: {
     color: "black",
     textDecoration: "none",
+    fontSize: "15px",
+    fontWeight: "bold",
     "&:hover": {
       color: "darkred",
-    },
+    }
   },
 }));
 
-function Footer() {
+function Footer(props) {
   const classes = useStyles();
+  const user = props.user;
 
   return (
     <div className={classes.root}>
       <CssBaseline />
       <footer className={classes.footer}>
-        <Container maxWidth="md">
+        <Container>
           <Typography variant="body1" className={classes.memberText}>
-            <span>
-              <a
-                className={classes.memberLink}
-                href="https://color-story.herokuapp.com/users/15"
-              >
-                Linus Schief
-              </a>
-            </span>
-            <span>・</span>
-            <span>
-              <a
-                className={classes.memberLink}
-                href="https://color-story.herokuapp.com/users/14"
-              >
-                Matt Leuer
-              </a>
-            </span>
-            <span>・</span>
-            <span>
-              <a
-                className={classes.memberLink}
-                href="https://color-story.herokuapp.com/users/4"
-              >
-                Chyna Davis
-              </a>
-            </span>
-            <span>・</span>
-            <span>
-              <a
-                className={classes.memberLink}
-                href="https://color-story.herokuapp.com/users/13"
-              >
-                Elizabeth Munoz
-              </a>
-            </span>
+            <Typography
+              className={classes.memberLink}
+              component={Link}
+              to={"/users/15"}
+            >
+              Linus Schief
+            </Typography>
+            <Typography>・</Typography>
+            <Typography
+              className={classes.memberLink}
+              component={Link}
+              to={"/users/14"}
+            >
+              Matt Leuer
+            </Typography>
+            <Typography>・</Typography>
+            <Typography
+              className={classes.memberLink}
+              component={Link}
+              to={"/users/4"}
+            >
+              Chyna Davis
+            </Typography>
+            <Typography>・</Typography>
+            <Typography
+              className={classes.memberLink}
+              component={Link}
+              to={"/users/13"}
+            >
+              Elizabeth Munoz
+            </Typography>
           </Typography>
           <Copyright />
         </Container>
